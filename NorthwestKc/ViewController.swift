0e465009c6dcccdd3118e6e2ac3da55472952d93
Peddi,Taraka Raviteja <s533904@ol10034m.nwmissouri.edu>
2019-03-13 16:17:06 -0500

    Initial Commit

---
+++ NorthwestKc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NorthwestKc
+//
+//  Created by Peddi,Taraka Raviteja on 3/13/19.
+//  Copyright © 2019 Peddi,Taraka Raviteja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
